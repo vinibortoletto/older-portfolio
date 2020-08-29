bc6b967e34efef745fe541e55d3424ba40b09f23
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2020-08-28 21:49:08 -0300

    add: footer layout and styles

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+// Images
+import logo from "../../images/icons/logo.svg";
+import shape from "../../images/illustrations/shape-bottom-mobile.svg";
+
+// Components
+import Button from "../../components/Button";
+
+// Styles
 import { Container } from "./styles";
 
 function Footer() {
-  return <Container>footer</Container>;
+  return (
+    <Container>
+      <div className="logo">
+        <img src={logo} alt="vinicius bortoletto's logo" />
+      </div>
+
+      <div className="social-icons">
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={["fab", "github-square"]} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon="envelope" />
+          </li>
+        </ul>
+      </div>
+
+      <div className="download">
+        <Button icon="file-download" text="Download CV" />
+      </div>
+
+      <div className="copyright">
+        <p>Created and designed by Vinicius Bortoletto</p>
+      </div>
+
+      <img src={shape} alt="abstract shape" className="shape" />
+    </Container>
+  );
 }
 
 export default Footer;
--- src/pages/Footer/styles.js
+++ src/pages/Footer/styles.js
@@ -1,3 +1,49 @@
 import styled from "styled-components";
 
-export const Container = styled.footer``;
+export const Container = styled.footer`
+  display: grid;
+  justify-items: center;
+
+  .logo img {
+    width: 10rem;
+    margin-bottom: 2.5rem;
+  }
+
+  .social-icons {
+    margin-bottom: 1.5rem;
+
+    ul {
+      display: flex;
+    }
+
+    li {
+      margin-right: 1.5rem;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+
+    svg {
+      color: var(--c-lGreen);
+      font-size: 1.75rem;
+    }
+  }
+
+  .download {
+    margin-bottom: 1rem;
+  }
+
+  .copyright {
+    color: var(--c-lGreen);
+    font-size: var(--fz-sm);
+    text-align: center;
+    margin-bottom: 10rem;
+  }
+
+  .shape {
+    /* width: 100%; */
+    margin: -6px -1rem;
+    width: calc(100% + 2rem);
+  }
+`;
